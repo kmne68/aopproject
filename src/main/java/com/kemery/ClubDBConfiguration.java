2541f25819c96a91217a7b2e169526ac7feab403
kmne6 <kmne6@192.168.77.163>
2018-04-19 19:53:55 -0500

    Initial commit of AOP project

---
+++ src/main/java/com/kemery/ClubDBConfiguration.java
@@ -0,0 +1,56 @@
+package com.kemery;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.object.MappingSqlQuery;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan(basePackages = {"com.kemery"})
+@EnableAspectJAutoProxy
+@EnableTransactionManagement
+public class ClubDBConfiguration {
+
+	@Bean
+	public DataSource dataSource() {
+		
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/club296?useSSL=false");
+		dataSource.setUsername("root");
+		dataSource.setPassword("");
+		
+		return dataSource;
+	}
+	
+	
+	@Bean
+	public MemberService memberService() {
+		
+		MemberServiceJdbcTxImplWithSpring bean = new MemberServiceJdbcTxImplWithSpring();
+		
+		bean.setDataSource(dataSource());
+		
+		return bean;
+		
+	}
+	
+	
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
+		transactionManager.setDataSource(dataSource());
+		return transactionManager;
+	}
+	
+}
+
---
+++ src/main/java/com/kemery/ExecutionOrderAfterThrowing.java
@@ -0,0 +1,20 @@
+package com.kemery;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * User: mertcaliskan
+ * Date: 25/06/14
+ */
+@Component
+@Aspect
+public class ExecutionOrderAfterThrowing {
+
+    @AfterThrowing(value = "within(com.kemery.MemberServiceJdbcTxImplWithSpring)", throwing = "t")
+    public void afterThrowing(JoinPoint joinPoint, Throwable t) {
+        System.out.println("===3 After throwing an exception: " + t.getMessage());
+    }
+}
